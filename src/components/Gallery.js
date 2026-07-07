06c74d9fe23e2d18736289d435227ac650f5af0d
Sofía De la Cueva <50554541+Vel0uria@users.noreply.github.com>
2026-07-06 21:30:26 -0600

    más obras al json y modificaciones a cloudinary y modificaciones a la lògica de la galería

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,7 @@
-import { cld, cldVideo } from "../utils/cloudinary.js";
+import { cld, cldThumb, cldVideo } from "../utils/cloudinary.js";
 
-img.src = cld(work.media.thumbnail, {
-  width: 400
-});
+nigredoImg.src = cldThumb("v1783392353/Nigredo/nigredo_thumbnail.png");
 
-video.src = cldVideo(work.media.video);
+//video.src = cldVideo(work.media.video);
+
+//https://res.cloudinary.com/dn1i6hdch/image/upload/c_thumb,w_200,g_face/v1783392353/Nigredo/nigredo_thumbnail.png
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+// src/utils/cloudinary.js
+
+import { CLOUD_NAME } from "../config.js";
+
+const BASE_URL = `https://res.cloudinary.com/${CLOUD_NAME}`;
+
+/**
+ * Genera URLs optimizadas para imágenes de Cloudinary
+ */
+export function cld(
+  path
+  //{ width = 1200, quality = "auto", format = "auto" } = {}
+) {
+  return `${BASE_URL}/image/upload/` + `${path}`;
+}
+
+export function cldThumb(path) {
+  return `${BASE_URL}/image/upload/c_thumb,w_200,g_face/` + `${path}`;
+}
+
+/**
+ * Genera URLs optimizadas para video
+ */
+export function cldVideo(path, { quality = "auto" } = {}) {
+  return `${BASE_URL}/video/upload/` + `${path}`;
+}
+
+//https://res.cloudinary.com/dn1i6hdch/image/upload/c_thumb,w_200,g_face/v1783392353/Nigredo/nigredo_thumbnail.png
